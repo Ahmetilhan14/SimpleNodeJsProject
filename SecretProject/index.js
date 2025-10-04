ba5210e2fd869924df917e4962ce9dd3fe23a805
Ahmet İlhan Sağlam <129795828+Ahmetilhan14@users.noreply.github.com>
2025-10-04 17:11:38 +0300

    Add files via upload
    Other simple API with Node.js 
    1-) Download file and open with any ide 
    2-) Open terminal and install npm and init 
    3-) Start with nodemon and look this simple project

---
+++ SecretProject/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import axios from "axios";
+
+
+const app=express();
+const port=3000;
+
+
+app.use(express.static("public"));
+
+app.get("/",async(req,res)=>{
+    try{
+        const result=await axios.get("https://secrets-api.appbrewery.com/random");
+        res.render("index.ejs",
+        {
+        secret: result.data.secret , 
+        user: result.data.username,});
+    }catch(error){
+       res.status(500).send("Internal Server Error");
+    }
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
+})
+
